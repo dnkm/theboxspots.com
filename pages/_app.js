528658201d65d27cc06a9e9f548e410cc7a6f0d6
YTG2G3 <scratchlover05@gmail.com>
2022-07-22 18:15:33 -0700

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Head from 'next/head';
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+    return (
+        <>
+            <Head>
+                <title>Box Spots</title>
+            </Head>
+
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp
